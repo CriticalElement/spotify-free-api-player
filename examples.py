15e9cd99b51885314981b3e8ce184f1d30cf75b0
CriticalElement <50345351+CriticalElement@users.noreply.github.com>
2020-12-02 21:39:52 -0600

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,4 +1,7 @@
 from experimental import SpotifyPlayer
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)-8s - %(name)-14s - %(message)s')
 
 spotifyplayer = SpotifyPlayer()
 spotifyplayer.command(spotifyplayer.add_to_queue('5wilF6g0r4VpNqQsV8fWKr'))
